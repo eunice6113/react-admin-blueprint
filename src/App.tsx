f70206386ddc80cd76cc0bed99e4d7b462ff09aa
lyk <lyk@ibksystem.co.kr>
2020-12-21 16:33:50 +0900

    admin.tsx project with blueprint library

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Intent, Spinner, Menu, MenuItem, Navbar, Alignment } from "@blueprintjs/core";
+import {TabsExample} from "./example/tabsExample";
+import {TreeExample} from "./example/treeExample";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const mySpinner = <Spinner intent={Intent.PRIMARY} />;
+
+    const myButton = React.createElement(Button, { intent: Intent.SUCCESS }, "button content");
+
+    return (
+        <div className="App">
+            <Menu>
+                <MenuItem text="Submenu">
+                    <MenuItem text="Child one" />
+                    <MenuItem text="Child two" />
+                    <MenuItem text="Child three" />
+                </MenuItem>
+            </Menu>
+
+
+            <Navbar>
+                <Navbar.Group align={Alignment.RIGHT}>
+                    <Navbar.Heading>Blueprint</Navbar.Heading>
+                    <Navbar.Divider />
+                    <Button className="bp3-minimal" icon="home" text="Home" />
+                    <Button className="bp3-minimal" icon="document" text="Files" />
+                </Navbar.Group>
+            </Navbar>
+
+
+            <TabsExample id='tabs'/>
+            
+            <TreeExample id='trees'/>
+
+        </div>
+    );
 }
 
 export default App;
